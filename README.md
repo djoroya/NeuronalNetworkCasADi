e6445282e3dc90279c891c919e80663e391d438b
Jesus Oroya <djoroya@deusto.es>
2020-07-22 09:53:46 +0200

    Primer Esquema

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 NeuronalNetworkCasADi
+
+## DEpendencias 
+
+- Es necesario tener casadi agregado en el path de MATLAB
+- Esta hecho en MATLAB R2020a (por si hay alguna cosa que no es compatible)
+## pic2data.m: 
+
+Script que transforma las imagenes en un data set manejable. Este dataset esta 'DigitDataset/DataSet.mat'
+    En cada componte contiene una base de datos de cada numero
+
+```matlab
+>> DataSet
+
+DataSet = 
+
+  1×10 struct array with fields:
+
+    YData
+    XData
+```
+
+Por ejemplo para extraer los datos del numero 2 
+
+```matlab
+>> DataSet(3)
+
+ans = 
+
+  struct with fields:
+
+    YData: 2
+    XData: [28×28×1000 double]
+```
+
+Para el número 2 hay 1000 ejemplos de matrix 28x28 donde esta dibujado a mano el numero 2.
+
+Para verolo podemos hacer 
+
+```
+figure(1)
+clf
+number = 2;
+for i = 1:16
+    subplot(4,4,i)
+    surf(DataSet(number+1).XData(:,:,i))
+    view(0,-90)
+    shading interp
+end
+```
+
+
+## NNCas.m: 
+
+Implementacion de Red Neuronal en CasaDi
+
+
+
---
+++ pic2data.m
@@ -0,0 +1,52 @@
+clear
+numbers = 0:9;
+
+
+for inumber = numbers
+    DataSet(inumber+1).YData = inumber;
+    path = fullfile('DigitDataset',num2str(inumber),'*.png');
+
+    files = ls(path);
+    % quitamos los enters y los remplazamos por espacios 
+    files = replace(files,newline,' ');
+    % remplazamos los retornos de carro
+    files = replace(files,'	',' ');
+    % dividimos la cadena en celdas 
+    files = strsplit(files,' ');
+    % quitamos el ultimo, siempre esta vacio
+    files = files(1:end-1);
+
+    %%
+
+    DataSet(inumber+1).XData = zeros(28,28,length(files));
+
+    it = 0;
+    for ifile = files
+        it = it + 1;
+        DataSet(inumber+1).XData(:,:,it) = imread(ifile{:});
+    end
+end
+
+%%
+figure(1)
+clf
+number = 1;
+for i = 1:16
+    subplot(4,4,i)
+    surf(DataSet(number).XData(:,:,i))
+    view(0,-90)
+    shading interp
+end
+%%
+figure(2)
+clf
+number = 2;
+for i = 1:16
+    subplot(4,4,i)
+    surf(DataSet(number).XData(:,:,i))
+    view(0,-90)
+    shading interp
+end
+%%
+save('DigitDataset/DataSet','DataSet')
+
